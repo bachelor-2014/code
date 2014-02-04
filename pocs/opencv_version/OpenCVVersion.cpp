b89f429d031e859ab1451cac3e33a233b99fded3
Mikkel Larsen <mikkellarsen1991@gmail.com>
2014-02-04 15:00:37 +0100

    Added OpenCV version POC

---
+++ pocs/opencv_version/OpenCVVersion.cpp
@@ -0,0 +1,10 @@
+#include <cv.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "OpenCV version: " << CV_VERSION << "\n";
+    return 0;
+}
