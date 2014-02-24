a7ee4e776aeb6b57dc9d658ad3ad13d6e2079761
Mikkel Larsen <milar@itu-dk>
2014-02-24 13:20:48 -0800

    Work on parsing JSON from C++

---
+++ splotbot/cpp/singlesteppermotor.cpp
@@ -0,0 +1,20 @@
+#include "singlesteppermotor.h"
+
+using namespace std;
+
+SingleStepperMotor::SingleStepperMotor() {
+    //strcopy(name, icJSON_GetObjectItem(document, "name").valuestring);
+    //printf("Name: %s", name);
+    printf("Test");
+}
+/*
+    public:
+        Motor(cJSON *document);
+
+    private:
+        char name;
+        char gpioMode1;
+        char gpioMode2;
+        char gpioStep;
+        char gpioSleep;
+        */
---
+++ splotbot/cpp/singlesteppermotor.h
@@ -0,0 +1,18 @@
+#ifndef SINGLESTEPPERMOTOR_H
+#define SINGLESTEPPERMOTOR_H 
+
+#include "instructionbuffer.h"
+
+class SingleStepperMotor {
+    public:
+        SingleStepperMotor();
+
+    private:
+        //char name[64];
+        //char gpioMode1[8];
+        //char gpioMode2[8];
+        //char gpioStep[8];
+        //char gpioSleep[8];
+};
+
+#endif
