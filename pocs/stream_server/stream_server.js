979db4030b392a862972ad5cc11c8491024dec71
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-02-25 23:10:49 +0100

    accepting input in stream server

--- pocs/stream_server/stream_server.js
+++ pocs/stream_server/stream_server.js
@@ -24,12 +24,34 @@ var server = require('http').Server(
 
     }
 
+    if(req.url == "/input" && req.method == "POST"){
+
+      var body = '';
+      req.on('data',function(data){
+        body += data;
+      })
+      req.on('end',function(){
+        console.log("GOT INPUT");
+        console.log(body);
+      })
+    }
+
+    if(req.method == "OPTIONS"){
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE,OPTIONS');
+      res.setHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+      res.setHeader('Access-Control-Allow-Credentials', true);
+
+      res.writeHead(200,"ok");
+      res.end();
+    }
   });
 
 var io = require('socket.io').listen(server, { log: false });
 
 function sendResponse(data,cb){
   io.sockets.emit('image_data', { image_data: base64Data } );
+  io.sockets.emit('image2_data', { image_data: base64Data } );
   //io.sockets.on('image_success', cb);
   cb();
 }
