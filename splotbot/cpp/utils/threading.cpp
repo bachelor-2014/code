0910ab8a29802e57385cd79c04ca423e670ab9d9
Mikkel Larsen <milar@itu-dk>
2014-03-11 13:47:41 -0700

    Cleaned up threading util to avoid race conditions also in environments capable of running multiple threads simultaneously

--- splotbot/cpp/utils/threading.cpp
+++ splotbot/cpp/utils/threading.cpp
@@ -4,20 +4,37 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-sem_t sem; //TODO Could be better
+// Struct containing the necessary parameters to the thread runner function
+typedef struct {
+    sem_t *sem;
+    function<void()> func;
+} ThreadParam;
 
+// Runner function for starting a new thread based on the given ThreadParam parameter
 void *runner(void *param) {
-    function<void()> func = *((function<void()> *) param);
-    sem_post(&sem);
+    ThreadParam tp = *((ThreadParam *) param);
+
+    function<void()> func = tp.func;
+    sem_t *sem = tp.sem;
+
+    sem_post(sem);
     func();
     pthread_exit(0);
 }
 
+// Runs the giving function in a separate thread
 void runAsThread(function<void()> func) {
+    sem_t sem;
     sem_init(&sem, 0, 0);
+
+    ThreadParam tp;
+    tp.sem = &sem;
+    tp.func = func;
+
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_t tid;
-    pthread_create(&tid, &attr, runner, (void *) &func);
+    pthread_create(&tid, &attr, runner, (void *) &tp);
+
     sem_wait(&sem);
 }
