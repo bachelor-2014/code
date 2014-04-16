2d0228403f8fb044deaec9dc06aefa1b7d742378
root <root@beaglebone.(none)>
2014-04-16 09:01:33 +0000

    global run

--- splotbot/run.sh
+++ splotbot/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -x
-socket="data/mendel.sock"
-mendel_cmd="../mendel.elf"
+socket="/tmp/mendel.sock"
+mendel_cmd="/usr/bin/mendel.elf"
 
 (cd node ; echo "" > $socket)
 (cd node ; tail -f $socket | $mendel_cmd &)
