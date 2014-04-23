3d518c7697eec0bad9cc265def3fcf290f35f726
root <root@beaglebone.(none)>
2014-04-23 06:23:32 +0000

    Suppressed mendel.elf debug prints in run.sh

--- splotbot/run.sh
+++ splotbot/run.sh
@@ -6,7 +6,7 @@ config="../basic_config.json"
 mendel_cmd="/usr/bin/mendel.elf"
 
 (cd node ; echo "" > $socket)
-(cd node ; tail -f $socket | $mendel_cmd &)
+(cd node ; tail -f $socket | $mendel_cmd > /dev/null 2>&1 &)
 (cd node ; ./run.js $config $socket)
 
 echo "ok"
