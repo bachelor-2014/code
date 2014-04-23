8b09e8e2b2bc50f56685091ccc6f2324e70e417f
mrob <mrob@itu.dk>
2014-04-23 17:13:20 +0200

    positionimagestitcher test data

--- splotbot/cpp/test/positionimagestitcher_test/positionimagestitcher_test.cpp
+++ splotbot/cpp/test/positionimagestitcher_test/positionimagestitcher_test.cpp
@@ -11,22 +11,39 @@ using namespace std;
 int main() {
     cout << "Constructing Image Stitcher" << endl;
     //Load test data
-    cv::Mat image = imread("100px.png", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image1 = imread("grab40_30.jpg", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image2 = imread("grab40_35.jpg", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image3 = imread("grab45_30.jpg", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image4 = imread("grab45_35.jpg", CV_LOAD_IMAGE_COLOR);
 
     vector<GrabbedImage> gv;
     GrabbedImage gi;
-    gi.positionX = 0;
-    gi.positionY = 0;
-    gi.image = image;
+    gi.positionX = 40;
+    gi.positionY = 30;
+    gi.image = image1;
     GrabbedImage gi2;
-    gi2.positionX = 1;
-    gi2.positionY = 0;
-    gi2.image = image;
+    gi2.positionX = 40;
+    gi2.positionY = 35;
+    gi2.image = image2;
+    GrabbedImage gi3;
+    gi3.positionX = 45;
+    gi3.positionY = 30;
+    gi3.image = image3;
+    GrabbedImage gi4;
+    gi4.positionX = 45;
+    gi4.positionY = 35;
+    gi4.image = image4;
 
     gv.push_back(gi);
     gv.push_back(gi2);
+    gv.push_back(gi3);
+    gv.push_back(gi4);
 
-    PositionImageStitcher pis(gv, NULL);
+    Camera camera("Cam", 0, "event");
+    camera.xStep = {24.915325033641395, 5.7030984481543534};
+    camera.yStep = {15.704089269213707, -2.6978784794984407};
+
+    PositionImageStitcher pis(gv, &camera);
     cout << "Testing stitching" << endl;
     cv::Mat stitched_image = pis.stitch();
     cv::namedWindow( "stitched", WINDOW_AUTOSIZE );
