375d7e229c47205c1309ee7bb0ebed66894c001c
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-05-06 13:45:16 +0200

    Everything but canvas works

---
+++ splotbot/client/app/scripts/directives/panel.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Directive definition for the 'Camera' type GUI control
+angular.module('clientApp')
+  .directive( "panel", function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      templateUrl: '/views/directives/panel.html',
+      link: function (scope, element, attrs) {
+        scope.heading = attrs.heading;
+        scope.show = false;
+
+        scope.toggle = function(){
+          if(scope.show)
+            scope.show = false;
+          else
+            scope.show = true;
+        }
+      }
+    }
+
+  })
