c3b3aeab04e30cde7c8407af5e855831dd5f9497
Christoffer Pedersen <stofferpedersen@gmail.com>
2014-05-07 12:45:53 +0200

    log buttons

--- splotbot/client/app/scripts/directives/logs.js
+++ splotbot/client/app/scripts/directives/logs.js
@@ -6,26 +6,36 @@ angular.module('clientApp')
     return {
       restrict: 'E',
       templateUrl: '/views/directives/logs.html',
-	  controller: function($scope,splotService){
+      controller: function($scope,splotService,$interval){
 
-		$scope.getLogs = function(){
-			splotService.getLogs().then(function(data){
-				$scope.logs = data.data;
-			});
-		}
+        $scope.getLogs = function(){
+          splotService.getLogs().then(function(data){
+            $scope.logs = data.data;
+          });
+        }
 
-		$scope.getLogLink = function(logname){
-			var url = splotService.URL + 'logs/' + logname;
-			return url;
-		};
+        $scope.getLogLink = function(logname){
+          var url = splotService.URL + 'logs/' + logname;
+          return url;
+        };
 
-		$scope.deleteLog = function(logname){
-			splotService.deleteLog(logname).then(function(data){
-				$scope.getLogs();
-			});
-		};
+        $scope.deleteLog = function(logname){
+          splotService.deleteLog(logname).then(function(data){
+            $scope.getLogs();
+          });
+        };
 
-		$scope.getLogs();
-	  }
+        $scope.clearAll = function(){
+          for(var l in $scope.logs){
+            $scope.deleteLog($scope.logs[l]);
+          }
+        }
+
+        $interval(function(){
+          console.log("Getting logs");
+          $scope.getLogs();
+        },15000);
+        $scope.getLogs();
+      }
     }
  })
